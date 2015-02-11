ade45e8e47c55977d11f21de7a5fd2ffbce587e4
Goalie7 <marcos13594@gmail.com>
2015-02-11 09:59:50 +0100

    Finish

--- app/src/main/java/perez/marcos/torturapp/MainActivity.java
+++ app/src/main/java/perez/marcos/torturapp/MainActivity.java
@@ -132,6 +132,7 @@ public class MainActivity extends Activity
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
